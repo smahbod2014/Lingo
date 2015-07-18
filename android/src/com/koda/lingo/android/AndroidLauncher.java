f2408058b88e50e1bdaef2526ad72df7099f5473
Sean Mahbod <smahbod2014@gmail.com>
2015-07-18 00:38:55 -0700

    Initial commit

---
+++ android/src/com/koda/lingo/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.koda.lingo.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.koda.lingo.Lingo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Lingo(), config);
+	}
+}
---
+++ desktop/src/com/koda/lingo/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.koda.lingo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.koda.lingo.Lingo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 360;
+        config.height = 600;
+        config.title = "Lingo";
+        config.resizable = false;
+		new LwjglApplication(new Lingo(), config);
+	}
+}
